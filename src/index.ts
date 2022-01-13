425a4159990ba4c02700ea61eaa6df42c148a275
Sebastian Schauer <233635+makaanneo@users.noreply.github.com>
2022-01-13 17:14:26 +0100

    first increment of new command to rename attachments.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,38 @@
 import joplin from 'api';
+import {
+  MenuItemLocation,
+  SettingItemType,
+  ToolbarButtonLocation
+} from 'api/types';
 
 joplin.plugins.register({
-	onStart: async function() {
-		console.info('Hello world. Test plugin started!');
-	},
+  onStart: async function () {
+    console.info('Plugin Apollo Started!');
+    await joplin.commands.register({
+      name: 'renameAttachment',
+      label: 'Rename Note Attachment',
+      iconName: 'far fa-copy',
+      execute: async () => {
+        const selectedNote = await joplin.workspace.selectedNote();
+        console.log(selectedNote);
+        let noteAttachments: any = null;
+        try {
+          noteAttachments = await joplin.data.get(
+            ['notes', selectedNote.id, 'resources'],
+            { fields: ['id', 'title',  'file_extension', 'filename']}
+          );
+          console.log(noteAttachments.items);
+        } catch (e) {
+          console.error('note resource retrieval error');
+          console.error(e);
+        }
+      }
+    });
+
+    await joplin.views.toolbarButtons.create(
+      'renameAttachment-button',
+      'renameAttachment',
+      ToolbarButtonLocation.EditorToolbar
+    );
+  }
 });
